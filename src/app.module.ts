f094bd0b96c3dfebe35e72c5ea8c6058aaf992dd
Francisco Everton Nascimento Serpa <evertonnascimento.serpa@gmail.com>
2021-12-06 18:20:27 -0300

    Estrutura das tabelas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TelephonesModule } from './telephones/telephones.module';
+import { AdressesModule } from './adresses/adresses.module';
+import { CategoriesModule } from './categories/categories.module';
+import { SubcategoriesModule } from './subcategories/subcategories.module';
+import { ProductsModule } from './products/products.module';
+import { ProductsNoSpecService } from './products--no-spec/products--no-spec.service';
+import { SellersModule } from './sellers/sellers.module';
 
 
 @Module({
-  imports: [UsersModule, TelephonesModule],
+  imports: [UsersModule, TelephonesModule, AdressesModule, CategoriesModule, SubcategoriesModule, ProductsModule, SellersModule],
   controllers: [],
-  providers: [],
+  providers: [ProductsNoSpecService],
 })
 export class AppModule {}
