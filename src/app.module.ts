f115e0b132fad3060fdab7c867a871c2fb3879c0
Francisco Everton Nascimento Serpa <evertonnascimento.serpa@gmail.com>
2021-12-10 17:18:19 -0300

    At arquivos

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { TelephoneModule } from './telephone/telephone.module';
 import { SubcategorieModule } from './subcategorie/subcategorie.module';
 import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
+import { CategoriesModule } from './categories/categories.module';
 
 
 @Module({
-  imports: [UserModule, TelephoneModule, SubcategorieModule, ProductModule, OrderModule,],
+  imports: [UserModule, TelephoneModule, SubcategorieModule, ProductModule, OrderModule, CategoriesModule,],
   controllers: [],
   providers: [PrismaService],
 })
