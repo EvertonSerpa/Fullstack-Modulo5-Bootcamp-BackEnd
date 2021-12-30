b10740eb63bc3e773fecfaac79969366120bb92b
Francisco Everton Nascimento Serpa <evertonnascimento.serpa@gmail.com>
2021-12-30 15:03:26 -0300

    Delete wishlist.entity.ts
    del arquivos com conflitos, mesclando com o arquivo  mais recente.

--- src/wishlists/entities/wishlist.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Prisma } from '@prisma/client';
-
-export class Wishlist implements Prisma.WishlistsUncheckedCreateInput {
-  id_wishlist?: string;
-  usersId_users: string;
-  sellerId_users?: string;
-  productsId_products?: string;
-}
